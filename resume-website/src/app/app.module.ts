c8801aac6e3f91cfbb96dfc43b66d6e9b6e10117
Logan Chantry <logan.chantry@ndus.edu>
2021-03-29 11:20:50 -0500

    Started working on the about-me component

--- resume-website/src/app/app.module.ts
+++ resume-website/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutMeComponent } from './about-me/about-me.component';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
